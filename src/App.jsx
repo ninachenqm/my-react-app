85350004108941ec538fbba6f5d3006ccbe9d479
Nina Chen <ninachenqm@gmail.com>
2025-04-02 00:35:21 -0500

    Each Square receives a value prop that will either be 'X', 'O', or null for empty squares.

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,28 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-function Square() {
-
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X');
-  }
-
-  return (
-    <button
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button>
-
-  );
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 
 export default function Board() {
-
   const [squares, setSquares] = useState(Array(9).fill(null));
-
   return (
     <>
       <div className="board-row">
-        <Square value={setSquares[0]} />
-        <Square value={setSquares[1]} />
-        <Square value={setSquares[2]} />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square value={setSquares[3]} />
-        <Square value={setSquares[4]} />
-        <Square value={setSquares[5]} />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square value={setSquares[6]} />
-        <Square value={setSquares[7]} />
-        <Square value={setSquares[8]} />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
-
-
 }
