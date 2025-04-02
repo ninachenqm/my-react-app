f37a62854519a8e5bb4b6631dac4e4749795a172
Nina Chen <ninachenqm@gmail.com>
2025-04-02 01:16:21 -0500

    add some state to Game component to track: which player, and the history of moves.

--- src/App.jsx
+++ src/App.jsx
@@ -57,6 +57,10 @@ export default function Board() {
 }
 
 export default function Game() {
+
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+
   return (
     <div className="game">
       <div className="game-board">
