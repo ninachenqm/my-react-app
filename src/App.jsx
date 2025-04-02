5d4d2585682fe3d4bfb6370140ce55e48fbbf972
Nina Chen <ninachenqm@gmail.com>
2025-04-02 01:37:15 -0500

    update the jumpTo function

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,7 @@ export default function Game() {
 
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
@@ -67,7 +68,8 @@ export default function Game() {
   }
 
   function jumpTo(nextMove) {
-    
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
