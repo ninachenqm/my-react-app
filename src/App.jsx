d1cd4f4331f379ef2437fa71c38ff0f7029d8a0d
Nina Chen <ninachenqm@gmail.com>
2025-04-01 23:14:19 -0500

    add handleClick to Square

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-function Square({value}) {
-  return <button className="square">{value}</button>;
+function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+
+  );
 }
 
 export default function Board() {
